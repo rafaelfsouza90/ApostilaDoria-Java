900339774f6fc88848dff9fe70353b528ce1792b
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-17 17:21:19 -0300

    Exemplo1_1

---
+++ Exemplo1_1.java
@@ -0,0 +1,12 @@
+package exercicios;
+public class Exemplo1_1{
+    
+    public static void main(String[]args){
+        
+        System.out.println("Hello World");
+    }
+    
+    
+    
+    
+}
